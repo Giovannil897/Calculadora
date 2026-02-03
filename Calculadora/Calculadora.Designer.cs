9a08e5f4e4d57f7eca7e0dc2966f4f020ec33d1d
fam florez <aramburo.florez2006@outlook.com>
2026-02-03 15:20:01 -0600

    se agrego un group box con las operaciones a realizar

--- Calculadora/Calculadora.Designer.cs
+++ Calculadora/Calculadora.Designer.cs
@@ -33,6 +33,12 @@
             VariableA = new TextBox();
             label2 = new Label();
             label1 = new Label();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // Calcular
@@ -77,18 +83,79 @@
             label1.TabIndex = 9;
             label1.Text = "Variable a";
             // 
-            // Form1
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(11, 143);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 141);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Operaciones";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(11, 21);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(54, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "suma";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(11, 46);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(50, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "resta";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(11, 71);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(94, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(11, 96);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(94, 19);
+            radioButton4.TabIndex = 3;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Calculadora
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(387, 450);
+            Controls.Add(groupBox1);
             Controls.Add(Calcular);
             Controls.Add(VariableB);
             Controls.Add(VariableA);
             Controls.Add(label2);
             Controls.Add(label1);
-            Name = "Form1";
+            Name = "Calculadora";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -100,5 +167,10 @@
         private TextBox VariableA;
         private Label label2;
         private Label label1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
     }
 }
