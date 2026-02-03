5383a43f05539d0b93c932c8616cd1da8eb3d3d2
fam florez <aramburo.florez2006@outlook.com>
2026-02-03 15:37:42 -0600

    se empezo con el formulario de burbuja ademas de empezar con el menu

--- Calculadora/Burbuja.Designer.cs
+++ Calculadora/Burbuja.Designer.cs
@@ -29,31 +29,60 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            Desordenados = new DataGridViewTextBoxColumn();
+            dataGridView2 = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Desordenados });
             dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.Size = new Size(240, 399);
             dataGridView1.TabIndex = 0;
+            // 
+            // Desordenados
+            // 
+            Desordenados.HeaderText = "Desordenados";
+            Desordenados.Name = "Desordenados";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1 });
+            dataGridView2.Location = new Point(532, 12);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(240, 399);
+            dataGridView2.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "Ordenados";
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
             // 
             // Burbuja
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView2);
             Controls.Add(dataGridView1);
             Name = "Burbuja";
             Text = "Form2";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Desordenados;
+        private DataGridView dataGridView2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
     }
 }
